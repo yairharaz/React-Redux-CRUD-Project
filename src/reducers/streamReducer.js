f56439b86c2e5e37678ecc42ae05113ebc57403b
yairharaz <yairharaz@gmail.com>
2021-03-09 12:24:48 +0200

    react-redux CRUD project

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import {
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAME
+} from '../actions/types';
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload, 'id')};
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload};
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload};
+        case DELETE_STREAME:
+            return _.omit(state, action.payload);
+        default:
+            return state;
+    }
+}  
